368898fa8937e2c1d0bea176bfbf3393c83f228c
Marc Gusmano <mgusmano@yahoo.com>
2017-04-24 10:10:51 -0400

    fixtd insex.js ref

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 // server.js
+var router = require('./routes/index');
+
 var jsonServer = require('json-server')
 var server = jsonServer.create()
-var router = jsonServer.router('db.json')
+//var router = jsonServer.router('db.json')
 var middlewares = jsonServer.defaults()
 
 server.use(middlewares)
