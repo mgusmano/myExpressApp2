d23a1cd07af3ab7bde1e9b822cd55c25a35a8285
Marc Gusmano <mgusmano@yahoo.com>
2017-04-24 11:39:24 -0400

    routes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var sortPlans = require('./routes/sortPlans');
 
 var app = express();
 
@@ -24,10 +25,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/sortPlans', sortPlans);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error('Not Found at all mjg');
   err.status = 404;
   next(err);
 });
