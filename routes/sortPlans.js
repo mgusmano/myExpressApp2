90d70ec7cbf81e11cae9ec689aec3b9aa49258ca
Marc Gusmano <mgusmano@yahoo.com>
2017-04-24 14:33:08 -0400

    json urlencoded

--- routes/sortPlans.js
+++ routes/sortPlans.js
@@ -9,7 +9,8 @@ const db = low('db2.json')
 const sortPlans = db.get('sortPlans')
 
 // create application/json parser 
-var jsonParser = bodyParser.json()
+// { type: 'application/*+json' }
+var jsonParser = bodyParser.json({ type: 'application/*+json' })
  
 // create application/x-www-form-urlencoded parser 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
