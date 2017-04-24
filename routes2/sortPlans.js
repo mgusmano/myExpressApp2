2983b9e0a89dd60eccea843824954259f609c63f
Marc Gusmano <mgusmano@yahoo.com>
2017-04-24 09:12:03 -0400

    no routes

---
+++ routes2/sortPlans.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  //res.send('respond with a resource');
+	res.json(
+		[
+			{
+				"deleted": true,
+				"sortPlanName": "plan one",
+				"planDesc": "the plan one description",
+				"notes": "quick note",
+				"id": 2
+			}
+		]
+	); 
+});
+
+//module.exports = router;
