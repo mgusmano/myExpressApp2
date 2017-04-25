711e4836c9460609600839a1a2f80159dd9780e6
Marc Gusmano <mgusmano@yahoo.com>
2017-04-25 11:11:39 -0400

    cleaned up

--- app.js
+++ app.js
@@ -1,77 +1,34 @@
 var express = require('express')
 var cors = require('cors')
-var path = require('path')
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express()
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(bodyParser.json({ type: 'application/json' }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
 
 var mongoose = require('mongoose')
 //var db = mongoose.connect('mongodb://localhost/bookAPI')
 var db = mongoose.connect('mongodb://mgusmano:Gusheandy1@cluster0-shard-00-00-gxxh8.mongodb.net:27017,cluster0-shard-00-01-gxxh8.mongodb.net:27017,cluster0-shard-00-02-gxxh8.mongodb.net:27017/bookAPI?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin')
 mongoose.connection.on('error', console.error.bind(console, 'connection error:'));
 mongoose.connection.on('connected', function () {console.log('Mongoose connection open');});
-
-// db.listCollections().toArray(function(err, collInfos) {
-// 	console.log(collinfos)
-// });
-
-//var bookRoute = require('./routes/book/bookRoutes');
-//app.get('/Books', bookRoute);
-//app.use('/api/Books', bookRoute)
-
-app.use('/api/Books', require('./routes/book/bookRoutes'))
-
-
-// app.use(cors())
-// var sortPlans = require('./routes/sortPlans');
-// app.use('/sortPlans', sortPlans);
-
+app.use('/Books', require('./routes/book/bookRoutes'))
 app.get('/notes', function(req, res) {
 	res.json({notes: "This is your notebook. Edit this to start saving your notes!"})
 })
+module.exports = app;
 
-// app.get('/', index);
-// app.get('/users', users);
+// var index = require('./routes/index');
+// var users = require('./routes/users');
 
-// app.post('/users', function (req, res) {
-// 	res.json(
-// 		[
-// 			{
-// 				"success": true
-// 			}
-// 		]
-// 	);
-// });
 
-// app.get('/users/:id', function (req, res) {
-// 	res.json(
-// 		[
-// 			{
-// 				"success": true
-// 			}
-// 		]
-// 	);
-// });
+//var path = require('path')
+//var favicon = require('serve-favicon');
+//var logger = require('morgan');
+//var cookieParser = require('cookie-parser');
+
+// var sortPlans = require('./routes/sortPlans');
+// app.use('/sortPlans', sortPlans);
 
 // // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
@@ -91,4 +48,25 @@ app.get('/notes', function(req, res) {
 //   res.render('error');
 // });
 
-module.exports = app;
+
+
+
+
+
+// db.listCollections().toArray(function(err, collInfos) {
+// 	console.log(collinfos)
+// });
+
+
+// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json({ type: 'application/json' }));
+//app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
