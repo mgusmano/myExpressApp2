6301cf7d03419e45eff50a74ff9c1ef2008afcb8
Marc Gusmano <mgusmano@yahoo.com>
2017-04-27 20:21:58 -0400

    clean up

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
 // var data = require('./utils/mongoose/data.js')
 // var Book = require('./models/mongoose/book');
 // var bookApi = require('./api/mongoose/book')(Book)
